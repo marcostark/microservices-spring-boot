f0b9312c7a038192fd5c461cba90b089672e0e1a
Marcos <mrcs.stark@gmail.com>
2020-07-12 22:57:59 -0300

    feat: Registra microservicos no eureka

--- store/src/main/java/dev/marcosouza/store/service/PurchaseService.java
+++ store/src/main/java/dev/marcosouza/store/service/PurchaseService.java
@@ -1,5 +1,6 @@
 package dev.marcosouza.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PurchaseService {
-	
+
+	final
+	RestTemplate client;
+
+	public PurchaseService(RestTemplate client) {
+		this.client = client;
+	}
+
 	public void makePurchase(PurchaseDTO purchase) {
 
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<SupplierDTO> exchange =
-				client.exchange("http://localhost:8081/info/" + purchase.getAddress().getUf(),
+				client.exchange("http://fornecedor:8081/info/" + purchase.getAddress().getUf(),
 				HttpMethod.GET, null, SupplierDTO.class);
 
 		System.out.println(exchange.getBody().getAddress());
