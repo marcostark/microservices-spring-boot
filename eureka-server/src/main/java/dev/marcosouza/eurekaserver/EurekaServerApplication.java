f01de0c4f8bdd606915cd77ad0de19ad26b2e8d3
Marcos <mrcs.stark@gmail.com>
2020-07-12 22:20:06 -0300

    chore(eureka-server):first commit

---
+++ eureka-server/src/main/java/dev/marcosouza/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package dev.marcosouza.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ eureka-server/src/test/java/dev/marcosouza/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcosouza.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
